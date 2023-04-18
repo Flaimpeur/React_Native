4e8c329466b81c1a43c3e7385bb6f79e70e183f3
Flaimpeur <114905674+Flaimpeur@users.noreply.github.com>
2023-04-18 17:11:01 +0200

    Après écriture text avec le prof

--- b2-rn/views/Test.js
+++ b2-rn/views/Test.js
@@ -1,6 +1,27 @@
-import { StatusBar } from "expo-status-bar";
-import { useState } from "react";
-import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import { StatusBar } from "expo-status-bar"
+import { useState } from "react"
+import { Button, StyleSheet, Text, TextInput, View } from "react-native"
+import * as FileSystem from 'expo-file-system'
+
+export const writefile = async (data) => {
+    // vérifier le contenu du data
+
+
+    try{
+        //vérifier si le fichier est vide
+        const fileInfo = await FileSystem.getInfoAsync(FileSystem.documentDirectory + 'file.txt')
+
+        if (fileInfo.exists) {
+            //Lire le fichier, et recuperer les donnees existantes
+            const fileContent = await readAsStringAsync(FileSystem.documentDirectory +'file.txt')
+            //Trouver un moyen pour ajouter le contenu passe en param au contenu existant
+        }else{
+            await FileSystem.writeAsStringAsync(FileSystem.documentDirectory +'file.txt', data)
+        }
+    } catch(error) {
+        console.log('Erreur non catche writefile util.js:', error)
+    }
+}
 
 function Test() {
     return (
